f743c76ae51f880b5253f00f5e63830921978678
Alex Tang <alxhtang@gmail.com>
2024-12-07 16:22:25 -0500

    a6

--- Kanbas/Assignments/dao.js
+++ Kanbas/Assignments/dao.js
@@ -1,21 +1,18 @@
-import Database from "../Database/index.js";
-export function findAssignmentsForCourse(courseId) {
-  const { assignments } = Database;
-  return assignments.filter((assignment) => assignment.course === courseId);
-  
+import model from "./model.js";
+
+export function updateAssignment(assignmentId, assignmentUpdates) {
+  return model.updateOne({ _id: assignmentId }, assignmentUpdates);
 }
-export function createAssignment(assignment) {
-  const newAssignment = { ...assignment, _id: Date.now().toString() };
-  Database.assignments = [...Database.assignments, newAssignment];
-  return newAssignment;
-}
+
 export function deleteAssignment(assignmentId) {
-  const { assignments } = Database;
-  Database.assignments = assignments.filter((assignment) => assignment._id !== assignmentId);
- }
- export function updaAssignment(assignmentId, assignmentUpdates) {
-  const { assignments } = Database;
-  const assignment = assignments.find((assignment) => assignment._id === assignmentId);
-  Object.assign(assignment, assignmentUpdates);
-  return assignment;
+  return model.deleteOne({ _id: assignmentId });
+}
+
+export function createAssignment(assignment) {
+  delete assignment._id;
+  return model.create(assignment);
+}
+
+export function findAssignmentsForCourse(courseId) {
+  return model.find({ course: courseId });
 }
--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,16 +1,15 @@
 import * as assignmentsDao from "./dao.js";
-export default function assignmentRoutes(app) {
 
-app.put("/api/assignments/:assignmentId", async(req, res) => {
+export default function AssignmentRoutes(app) {
+  app.put("/api/assignments/:assignmentId", async (req, res) => {
     const { assignmentId } = req.params;
     const assignmentUpdates = req.body;
     const status = await assignmentsDao.updateAssignment(assignmentId, assignmentUpdates);
     res.send(status);
-});
-    
- app.delete("/api/assignments/:assignmentId", async(req, res) => {
-   const { assignmentId } = req.params;
-   const status = await assignmentsDao.deleteAssignment(assignmentId);
-   res.send(status);
- });
+  });
+  app.delete("/api/assignments/:assignmentId", async (req, res) => {
+    const { assignmentId } = req.params;
+    const result = await assignmentsDao.deleteAssignment(assignmentId);
+    res.send(result.deletedCount === 1 ? 200 : 500);
+  });
 }
